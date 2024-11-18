be711272180947283e8a477f9fd7b48843872ab3
mihailcenov <mihailceov@gmail.com>
2024-11-18 19:48:53 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+
+def main():
+    print("Hello world!")
+    print("This is first program in Python here")
+    for i in range(5):
+        print(f"i = {i}")
+
+if __name__ == "__main__":
+    main()
